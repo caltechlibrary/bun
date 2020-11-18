4db0efadf14ebf62b894353da5c89893e3232c0e
mhucka <mhucka@caltech.edu>
2020-11-18 12:26:29 -0800

    Rename and improve

---
+++ demos/basic_demo.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+
+import os
+import sys
+
+try:
+    thisdir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(thisdir, '..'))
+except:
+    sys.path.append('..')
+
+from quiche import UI, inform, warn, alert, alert_fatal
+
+# Initialize Quiche ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ui = UI('demo', 'A demonstration of Quiche')
+ui.start()
+
+# Demo some Quiche functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+inform('This is an informational message')
+warn('This is a warning')
+alert('This is an alert')
+alert_fatal('This is a fatal alert')
+
+# Say goodbye ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+inform('Done')
