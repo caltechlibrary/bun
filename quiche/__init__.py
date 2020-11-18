805679c4461f2855b8f0e236f00d9e8c93be5b98
Michael Hucka <mhucka@caltech.edu>
2020-11-18 12:22:43 -0800

    Add __init__.py

---
+++ quiche/__init__.py
@@ -0,0 +1,46 @@
+'''
+quiche: Quintessential UI Class HiErarchy
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2018-2020 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+# Package metadata ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '0.0.1'
+__description__ = 'Quintessential UI Class HiErarchy'
+__url__         = 'https://github.com/caltechlibrary/quiche'
+__author__      = 'Michael Hucka'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause'
+
+
+# Exports.
+# .............................................................................
+
+from .ui import *
+from .cli import CLI
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
