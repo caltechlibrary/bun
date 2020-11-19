f3615365d7f6db07f971c21841edd67baf480b7b
mhucka <mhucka@caltech.edu>
2020-11-18 19:15:04 -0800

    Update version

--- quiche/__init__.py
+++ quiche/__init__.py
@@ -21,7 +21,7 @@ file "LICENSE" for more information.
 #  |    by the Makefile. Manual changes to these values will be lost.      |
 #  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
 
-__version__     = '0.0.1'
+__version__     = '0.0.2'
 __description__ = 'A small Python package for a basic user interface'
 __url__         = 'https://github.com/caltechlibrary/quiche'
 __author__      = 'Michael Hucka'
